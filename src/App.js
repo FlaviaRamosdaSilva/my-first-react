91a502bfc81f5cdf18125f21339d24fa77934267
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-08-23 22:28:36 -0300

    iniciando-front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button} from "./styles"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const hello = "Hello React"
+
+const App = () => {
+  return <Container>
+<Image />
+<ContainerItens>
+  <H1>Olá!</H1>
+  <InputLabel>Nome</InputLabel>
+  <Input placeholder='Nome' />
+  <InputLabel>Idade</InputLabel>
+  <Input placeholder='Idade' />
+  <Button>Cadastrar</Button>
+</ContainerItens>
+
+  </Container>
 }
-
-export default App;
+export default App
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+import Background from "./assets/fundo.svg"
+
+export const Container = styled.div`
+    background: url("${Background}");
+    background-size: cover;
+`
+
+export const Image = styled.img`
+
+`
+export const ContainerItens = styled.div`
+
+`
+
+export const H1 = styled.h1`
+
+`
+export const InputLabel = styled.p`
+
+`
+
+export const Input = styled.input`
+
+`
+
+export const Button = styled.button`
+
+`
