981f1a67f1530a12333a26234b647795ead83f29
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-08-29 22:42:05 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Container, Image, ContainerItens, H1, InputLabel, Input, Button} from "./styles"
 
-const hello = "Hello React"
+import People from "./assets/Image1.svg"
+import Arrow from "./assets/seta.svg"
+
 
 const App = () => {
   return <Container>
-<Image />
+<Image alt="logo-imagem" src={People}/>
 <ContainerItens>
   <H1>Olá!</H1>
   <InputLabel>Nome</InputLabel>
   <Input placeholder='Nome' />
   <InputLabel>Idade</InputLabel>
   <Input placeholder='Idade' />
-  <Button>Cadastrar</Button>
+  <Button>Cadastrar <img alt='seta' src={Arrow}/></Button>
 </ContainerItens>
 
   </Container>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './globalStyle';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<><App /> < GlobalStyle /> </>, document.getElementById('root'));
 
 
