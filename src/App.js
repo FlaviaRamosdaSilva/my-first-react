0dafd52ccaf1718e4c7b096ff806905b6c536214
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2023-09-08 20:28:17 -0300

    Spread_operator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react'
-import { Container, Image, ContainerItens, H1, InputLabel, Input, Button} from "./styles"
-
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User} from "./styles"
+import { useState } from 'react'
 
 
 import People from "./assets/Image1.svg"
 import Arrow from "./assets/seta.svg"
-
+import Trash from "./assets/lixeira.svg"
 
 function App() {
-const users = [
-  {id: Math.random(), name: "Rodolfo", age: 28},
-  {id: Math.random(), name: "Flávia", age: 34},
+const [users, setUsers] = useState([]);
+const [name, setName] = useState();
+const [age, setAge] = useState();
+ // {id: Math.random(), name: "Rodolfo", age: 28},
+ // {id: Math.random(), name: "Flávia", age: 34},
  
-];
+function changeInputName(event){
+setName(event.target.value)
+};
+
+function changeInputAge(event){
+setAge(event.target.value)
+};
+
+function addNewUser(){
+setUsers([ ...users,{id: Math.random(), name, age}])
+};
+
 
   return ( 
     <Container>
@@ -20,16 +33,19 @@ const users = [
 <ContainerItens>
   <H1>Olá!</H1>
   <InputLabel>Nome</InputLabel>
-  <Input placeholder='Nome' />
+  <Input onChange={changeInputName} placeholder='Nome' />
   <InputLabel>Idade</InputLabel>
-  <Input placeholder='Idade' />
-  <Button>Cadastrar <img alt='seta' src={Arrow}/></Button>
+  <Input onChange={changeInputAge} placeholder='Idade' />
+  <Button onClick={addNewUser}>
+    Cadastrar <img alt='seta' src={Arrow}/>
+    </Button>
 
   <ul>
     {users.map((user) => (
-      <li key={user.id}>
-        {user.name} - {user.age}
-    </li>))}
+      <User key={user.id}>
+        <p>{user.name}</p> <p>{user.age}</p>
+        <button><img src={Trash} alt='lata-de-lixo'/></button>
+    </User>))}
   </ul>
 </ContainerItens>
 
--- src/styles.js
+++ src/styles.js
@@ -100,3 +100,34 @@ gap: 20px;
     opacity: 0.5;
 }
 `
+
+export const User = styled.li`
+display: flex;
+justify-content: space-around;
+align-items: center;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+width: 342px;
+height: 58px;
+border: none;
+outline: none;
+
+margin-top: 20px;
+
+p{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 28px;
+    color: #ffffff;
+}
+
+button{
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+
+`
